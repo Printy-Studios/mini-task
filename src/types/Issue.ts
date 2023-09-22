af86e3d51d2dd1fb84aa59ae0042d1cea85c072b
JorensM <jorensmerenjanu@gmail.com>
2023-09-22 10:37:54 +0300

    feat(types): add IssuePriority and IssueStatus types and add them to IssueMetadata

--- src/types/Issue.ts
+++ src/types/Issue.ts
@@ -1,3 +1,13 @@
+export type IssuePriority = {
+    value: number | string,
+    label?: string
+}
+
+export type IssueStatus = {
+    value: number | string,
+    label?: string
+}
+
 export type Issue = {
     name: string,
     description?: string,
@@ -6,6 +16,6 @@ export type Issue = {
 
 export type IssueMetadata = {
     id: string,
-    priority?: number,
-    status?: string
+    priority?: IssuePriority,
+    status?: IssueStatus
 }
