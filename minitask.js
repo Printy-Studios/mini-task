ec1f64bb408587a8a24a7995a6865b616c5a4c36
JorensM <jorensmerenjanu@gmail.com>
2023-09-30 15:39:49 +0300

    chore(testing): add minitask.js for testing

---
+++ minitask.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "issues-path": "issues",
+    "plugins": {
+        "txt-format": true,
+        "markdown-renderer": {
+            "enabled": true,
+            "test-property": 123
+        }
+    }
+}
