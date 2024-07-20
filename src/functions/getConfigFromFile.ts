32fd05741928570216d3bc729bded5f19e0c66d6
JorensM <jorensmerenjanu@gmail.com>
2024-07-20 15:31:28 +0300

    refactor: make getconfigFromFile synchronous

--- src/functions/getConfigFromFile.ts
+++ src/functions/getConfigFromFile.ts
@@ -16,7 +16,11 @@ import JSONValidator from './JSONValidator'
 
 const logger = new Logger(true, 'Log')
 
-export default async function getConfigFromFile() {
+/**
+ * Get config from file
+ * @returns 
+ */
+export default function getConfigFromFile() {
 
     const config_file_json = findFileUp('minitask.json', process.env.INIT_CWD, true, 5)
     const config_file_js = findFileUp('minitask.js', process.env.INIT_CWD, false, 5)
@@ -30,7 +34,10 @@ export default async function getConfigFromFile() {
     Throw error if neither are found 
     */
     if ( config_file_js ) {
-        config = (await import(path.join(config_file_js.path, 'minitask.js'))).default
+        throw new Error('For now .js config is not supported, please use .json');
+        /* 
+         config = (await import(path.join(config_file_js.path, 'minitask.js'))).default
+        Need a different approach that doens't use await so we don't have to make all dependant functions async*/
         file_path = config_file_js.path
         logger.log('Found minitask.js config file at ' + file_path)
     } else if (config_file_json) {
